f75160ade164a7ff5e3034973db6ab69db23589b
Dike Mahoko <dike@uzinduziafrica.com>
2026-04-28 18:28:22 +0200

    fix vercel routing config fix1

--- missCherryui/src/app/app.routes.ts
+++ missCherryui/src/app/app.routes.ts
@@ -1,42 +1,53 @@
 import { Routes } from '@angular/router';
 import { DashboardLayout } from './layout/dashboard-layout/dashboard-layout';
 import { AuthLayout } from './layout/auth-layout/auth-layout';
-import { VerificationHistory } from './pages/verification-history/verification-history';
 import { VerifyBatch } from './pages/verify-batch/verify-batch';
 
-export const routes: Routes = [{
-    path:'verify/:batchId',
-    component:VerifyBatch
-},
+export const routes: Routes = [
+  // PUBLIC VERIFY ROUTE (no login required)
   {
-        path:'',
-        component:DashboardLayout,
-        children:[
-            {
-                path:'',
-                redirectTo:'dashboard',
-                pathMatch:'full',
-            },
-            {
-                path:'dashboard',
-                
-                loadChildren:()=>import('./modules/dashboard/dashboard-module').then(m=>m.DashboardModule)
-            }
-        ]
-    },
-    {
-                path:'',
-        component:AuthLayout,
-        children:[
-            {
-                path:'',
-                redirectTo:'auth',
-                pathMatch:'full',
-            },
-            {
-                path:'auth',
-                loadChildren:()=>import('./modules/auth/auth-module').then(m=>m.AuthModule)
-            }
-        ]
-    }
+    path: 'verify/:batchId',
+    component: VerifyBatch,
+  },
+
+  // AUTH FIRST
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'auth',
+        pathMatch: 'full',
+      },
+      {
+        path: 'auth',
+        loadChildren: () =>
+          import('./modules/auth/auth-module').then(
+            (m) => m.AuthModule
+          ),
+      },
+    ],
+  },
+
+  // DASHBOARD AFTER LOGIN
+  {
+    path: 'dashboard',
+    component: DashboardLayout,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./modules/dashboard/dashboard-module').then(
+            (m) => m.DashboardModule
+          ),
+      },
+    ],
+  },
+
+  // FALLBACK
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
 ];
