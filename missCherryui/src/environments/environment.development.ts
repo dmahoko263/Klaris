2d5bada7dd49c3c97b0bb6a9f653bf872cc2e224
Dike Mahoko <dike@uzinduziafrica.com>
2026-04-28 17:57:28 +0200

    dded vercel.json

--- missCherryui/src/environments/environment.development.ts
+++ missCherryui/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  // apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'http://192.168.100.124:5000/api',
   ipfsGateway: 'https://ipfs.io/ipfs'
 };
